ac75368706d05cd58b1dabda89def9e24b424bb1
Aleilson Gomes de Cerqueira <aleilson.cerqueira@compasso.com.br>
2022-02-11 14:50:18 -0300

    Fix error deploy

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { AiOutlineWhatsApp, AiOutlineMenu } from "react-icons/ai";
 
 import './styles.scss'
